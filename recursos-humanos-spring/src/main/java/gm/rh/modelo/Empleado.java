7fcd52c66de4fd1da8bf6651f7d1cfcbc1aa3e03
CloroDev <aquinoalejandro666@gmail.com>
2025-12-10 10:58:16 -0300

    feat: :sparkles: Se añade servidor y cliente al proyecto
    Ya no formara parte del curso al respecto

---
+++ recursos-humanos-spring/src/main/java/gm/rh/modelo/Empleado.java
@@ -0,0 +1,24 @@
+package gm.rh.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data // GENERA GETTER Y SETTER AUTOMATICOOO
+@NoArgsConstructor // CREA CONTRUCTOR VACIO
+@AllArgsConstructor // CREA CONSTRUCTOR CON TODITO
+@ToString // CREA UN TOSTRING Q ES PARA QUE SE MUESTRE TODITO EN UN STRING
+public class Empleado {
+    @Id // INDICA QUE ES LA PRIMARY KEY
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // GENERA UN ID AUTOINCREMENTABLE
+    Integer idEmpleado;
+    String nombre;
+    String departamento;
+    Double sueldo;
+}
