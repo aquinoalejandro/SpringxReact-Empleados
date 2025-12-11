942fe0b9bf16e77066f5899a5688b210442fe925
CloroDev <aquinoalejandro666@gmail.com>
2025-12-11 07:41:03 -0300

    feat: :sparkles: Ahora se muestran los empleados en la tabla

--- recursos-humanos-react/src/components/ListadoEmpleados.tsx
+++ recursos-humanos-react/src/components/ListadoEmpleados.tsx
@@ -1,19 +1,19 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
-
   export default function ListadoEmpleados() {
-  const [Empleados, setEmpleados] = useState([]);
-
-
+  const [Empleados, setEmpleados] = useState([{
+    idEmpleado: 0,
+    nombre: "",
+    departamento: "",
+    sueldo: 0
+  }]);
 
   useEffect(() => {
     axios.get("http://localhost:8081/rh-app/empleados")
       .then((response) => {
         setEmpleados(response.data);
         console.log(Empleados);
-
-
       })
       .catch((error) => {
         console.log(error);
@@ -24,43 +24,28 @@ import axios from 'axios'
     console.log(Empleados);
   }, [Empleados]);
 
-
-
-
   return (
     <div className='continer' style={{width: '100%', display: 'flex'}}>
-    <table className="table">
-      <thead>
-        <tr>
-          <th scope="col">Id</th>
-          <th scope="col">Empleado</th>
-          <th scope="col">Departmento</th>
-          <th scope="col">Sueldo</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td>John</td>
-          <td>Doe</td>
-          <td>@social</td>
-        </tr>
-      </tbody>
-  </table>
-
-
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Empleado</th>
+            <th scope="col">Departmento</th>
+            <th scope="col">Sueldo</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Empleados.map((empleado) => (
+            <tr key={empleado.idEmpleado}>
+              <th scope="row">{empleado.idEmpleado}</th>
+              <td>{empleado.nombre}</td>
+              <td>{empleado.departamento}</td>
+              <td>{empleado.sueldo}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
